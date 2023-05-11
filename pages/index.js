9c21caabe44b49e764377c806a9b4d5c9770bcc2
AgaOras <agamamedov69@gmail.com>
2023-05-11 19:22:12 +0300

    1

---
+++ pages/index.js
@@ -0,0 +1,134 @@
+const vh = window.innerHeight / 100;
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+// Deprag W52
+let Info__DepragW52 = document.querySelector('#Info__Deprag-W52');
+let btnOpen__DepragW52 = document.querySelector('#btn-open__Deprag-W52');
+let btnClose__DepragW52 = document.querySelector('#btn-close__Deprag-W52');
+
+btnOpen__DepragW52.onclick = function() {
+  Info__DepragW52.style.display = "flex";
+}
+
+btnClose__DepragW52.onclick = function() {
+  Info__DepragW52.style.display = "";
+}
+
+// Deprag W42
+let Info__DepragW42 = document.querySelector('#Info__Deprag-W42');
+let btnOpen__DepragW42 = document.querySelector('#btn-open__Deprag-W42');
+let btnClose__DepragW42 = document.querySelector('#btn-close__Deprag-W42');
+
+btnOpen__DepragW42.onclick = function() {
+  Info__DepragW42.style.display = "flex";
+}
+
+btnClose__DepragW42.onclick = function() {
+  Info__DepragW42.style.display = "";
+}
+
+// Deprag W40
+let Info__DepragW40 = document.querySelector('#Info__Deprag-W40');
+let btnOpen__DepragW40 = document.querySelector('#btn-open__Deprag-W40');
+let btnClose__DepragW40 = document.querySelector('#btn-close__Deprag-W40');
+
+btnOpen__DepragW40.onclick = function() {
+  Info__DepragW40.style.display = "flex";
+}
+
+btnClose__DepragW40.onclick = function() {
+  Info__DepragW40.style.display = "";
+}
+
+// Deprag P7
+let Info__DepragP7 = document.querySelector('#Info__Deprag-P7');
+let btnOpen__DepragP7 = document.querySelector('#btn-open__Deprag-P7');
+let btnClose__DepragP7 = document.querySelector('#btn-close__Deprag-P7');
+
+btnOpen__DepragP7.onclick = function() {
+  Info__DepragP7.style.display = "flex";
+}
+
+btnClose__DepragP7.onclick = function() {
+  Info__DepragP7.style.display = "";
+}
+
+// Deprag S7
+let Info__DepragS7 = document.querySelector('#Info__Deprag-S7');
+let btnOpen__DepragS7 = document.querySelector('#btn-open__Deprag-S7');
+let btnClose__DepragS7 = document.querySelector('#btn-close__Deprag-S7');
+
+btnOpen__DepragS7.onclick = function() {
+  Info__DepragS7.style.display = "flex";
+}
+
+btnClose__DepragS7.onclick = function() {
+  Info__DepragS7.style.display = "";
+}
+
+// Deprag PL7
+let Info__DepragPL7 = document.querySelector('#Info__Deprag-PL7');
+let btnOpen__DepragPL7 = document.querySelector('#btn-open__Deprag-PL7');
+let btnClose__DepragPL7 = document.querySelector('#btn-close__Deprag-PL7');
+
+btnOpen__DepragPL7.onclick = function() {
+  Info__DepragPL7.style.display = "flex";
+}
+
+btnClose__DepragPL7.onclick = function() {
+  Info__DepragPL7.style.display = "";
+}
+
+//==========================================================================
+
+// Aoralscan 3 Wireless
+let Info__Aoralscan3Wireless = document.querySelector('#Info__Aoralscan3-Wireless');
+let btnOpen__Aoralscan3Wireless = document.querySelector('#btn-open__Aoralscan3-Wireless');
+let btnClose__Aoralscan3Wireless = document.querySelector('#btn-close__Aoralscan3-Wireless');
+
+btnOpen__Aoralscan3Wireless.onclick = function() {
+  Info__Aoralscan3Wireless.style.display = "flex";
+}
+
+btnClose__Aoralscan3Wireless.onclick = function() {
+  Info__Aoralscan3Wireless.style.display = "";
+}
+
+// AutoScan-DS-EX-Pro
+let Info__AutoScanDSEXPro = document.querySelector('#Info__AutoScan-DS-EX-Pro');
+let btnOpen__AutoScanDSEXPro = document.querySelector('#btn-open__AutoScan-DS-EX-Pro');
+let btnClose__AutoScanDSEXPro = document.querySelector('#btn-close__AutoScan-DS-EX-Pro');
+
+btnOpen__AutoScanDSEXPro.onclick = function() {
+  Info__AutoScanDSEXPro.style.display = "flex";
+}
+
+btnClose__AutoScanDSEXPro.onclick = function() {
+  Info__AutoScanDSEXPro.style.display = "";
+}
+
+// AutoScan-DS-EX Pro-H
+let Info__AutoScanDSEXPro_H = document.querySelector('#Info__AutoScan-DS-EX-Pro-H');
+let btnOpen__AutoScanDSEXPro_H = document.querySelector('#btn-open__AutoScan-DS-EX-Pro-H');
+let btnClose__AutoScanDSEXPro_H = document.querySelector('#btn-close__AutoScan-DS-EX-Pro-H');
+
+btnOpen__AutoScanDSEXPro_H.onclick = function() {
+  Info__AutoScanDSEXPro_H.style.display = "flex";
+}
+
+btnClose__AutoScanDSEXPro_H.onclick = function() {
+  Info__AutoScanDSEXPro_H.style.display = "";
+}
+
+// Super Vision
+let Info__SuperVision = document.querySelector('#Info__SuperVision');
+let btnOpen__SuperVision = document.querySelector('#btn-open__SuperVision');
+let btnClose__SuperVision = document.querySelector('#btn-close__SuperVision');
+
+btnOpen__SuperVision.onclick = function() {
+  Info__SuperVision.style.display = "flex";
+}
+
+btnClose__SuperVision.onclick = function() {
+  Info__SuperVision.style.display = "";
+}
